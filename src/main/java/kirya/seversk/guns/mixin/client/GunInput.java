8c3eec4f13afa94191c6a0b1075d2b516a7aed79
Kirill <scriptythememer@gmail.com>
2026-02-11 20:09:25 +0700

    remove unnecessary imports

--- src/main/java/kirya/seversk/guns/mixin/client/GunInput.java
+++ src/main/java/kirya/seversk/guns/mixin/client/GunInput.java
@@ -1,24 +1,18 @@
 package kirya.seversk.guns.mixin.client;
 
-import com.mojang.authlib.minecraft.client.MinecraftClient;
-import com.mojang.blaze3d.systems.TimerQuery;
-import kirya.seversk.guns.SeverskGuns;
+
 import kirya.seversk.guns.items.GunItem;
-import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
 import org.jspecify.annotations.Nullable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Minecraft.class)
 public class GunInput {
