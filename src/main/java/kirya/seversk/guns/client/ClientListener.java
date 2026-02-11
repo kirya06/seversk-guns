c040c1443c06dabd5c8a94e181ab045dd5493b90
Kirill <scriptythememer@gmail.com>
2026-02-11 21:35:53 +0700

    ModKeybinds instead of creating keybinds directly in client entrypoint ClientListener class for listening to client tick events (should move all input related stuff to these callbacks)

---
+++ src/main/java/kirya/seversk/guns/client/ClientListener.java
@@ -0,0 +1,14 @@
+package kirya.seversk.guns.client;
+
+import net.minecraft.client.Minecraft;
+
+/// Responsible for listening to client tick events and do stuff. Mainly input related.
+public class ClientListener {
+
+    public static void onStartTick(Minecraft client) {
+    }
+
+    public static void onEndTick(Minecraft client) {
+    }
+
+}
