76a000918e996f2fa71364570628ea2a1caa68a8
Kirill <scriptythememer@gmail.com>
2026-02-13 21:24:20 +0700

    inserting ammo into mags logic (very barebones)

--- src/main/java/kirya/seversk/guns/items/ammunition/MagItem.java
+++ src/main/java/kirya/seversk/guns/items/ammunition/MagItem.java
@@ -1,8 +1,14 @@
 package kirya.seversk.guns.items.ammunition;
 
+import kirya.seversk.guns.SeverskGuns;
 import kirya.seversk.guns.items.ModComponents;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.SlotAccess;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ClickAction;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
 public class MagItem extends Item {
 
@@ -20,5 +26,48 @@ public class MagItem extends Item {
     public MagItem withCapacity(int capacity) {
         maxCapacity = capacity;
         return this;
+    }
+
+    public MagComponent getMagComponent(ItemStack mag) {
+        if (!(mag.getItem() instanceof MagItem))
+            throw new  IllegalArgumentException(mag.toString());
+
+        return mag.get(ModComponents.MAG);
+    }
+
+    /// returns true if the ammo was successfully consumed.
+    public boolean consumeAmmo(ItemStack mag) {
+        return true;
+    }
+
+    @Override
+    public boolean overrideOtherStackedOnMe(ItemStack mag, ItemStack otherItemStack, Slot slot, ClickAction clickAction, Player player, SlotAccess slotAccess) {
+        if (otherItemStack.getItem() instanceof AmmoItem) {
+            otherItemStack.setCount(0);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean overrideStackedOnOther(ItemStack itemStack, Slot slot, ClickAction click, Player player) {
+        ItemStack slotStack = slot.getItem();
+
+        if (slotStack.getItem() instanceof AmmoItem) {
+
+            // consume the whole ammo stack
+            if (click == ClickAction.SECONDARY) {
+                slotStack.setCount(0);
+                return true;
+            }
+        }
+
+        return false;
+        // return super.overrideStackedOnOther(itemStack, slot, clickAction, player);
+    }
+
+    private int addToMag(ItemStack mag, ItemStack ammo) {
+        return 0;
     }
 }
